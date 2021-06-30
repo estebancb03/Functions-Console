64f863ecf3e07239b18d327866130a519ea10c70
estebancb03 <estebanr.castaneda03@gmail.com>
2021-06-30 01:04:57 -0600

    Main modificable para tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include "FuncionBooleana.h"
 using namespace std;
 
 int main() {
+	bool a, b, c;
+	string formula = "";
+	cout << "f(a,b,c) = "; cin >> formula; cout << endl;
+	cout << "Valor A = "; cin >> a; cout << endl;
+	cout << "Valor B = "; cin >> b; cout << endl;
+	cout << "Valor C = "; cin >> c; cout << endl;	
+	FuncionBooleana *funcion = new FuncionBooleana(formula, a, b, c);
+	funcion -> seleccionarMinterminos();
+	funcion -> getListaMinterminos() -> imprimirLista();
 	
 	return 0;
 }
