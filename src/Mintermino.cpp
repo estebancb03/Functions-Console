9b5a345e2faaf3d5fef21c0b0a0272fe63c8762b
estebancb03 <estebanr.castaneda03@gmail.com>
2021-06-29 23:59:37 -0600

    Creacion del metodo evaluar de la clase Mintermino

--- src/Mintermino.cpp
+++ src/Mintermino.cpp
@@ -26,3 +26,26 @@ void Mintermino :: cambiarValoresVerdad(bool introducidos[], bool determinados[]
         }
     }
 }
+
+void Mintermino :: evaluar() {
+    bool valoresIntroducidos[2];
+    bool valoresDeterminados[longitud(formulaMintermino)];
+    valoresIntroducidos[0] = introducidoA;
+    valoresIntroducidos[1] = introducidoB;
+    valoresIntroducidos[2] = introducidoC;
+    for(int i = 0; i < formulaMintermino.length(); i++) {
+        if(i < formulaMintermino.length() - 1 && !isalpha(formulaMintermino[i + 1])) {
+            valoresDeterminados[i] = false;
+            i++;
+        }
+        else
+            valoresDeterminados[i] = true;
+    }
+    cambiarValoresVerdad(valoresIntroducidos, valoresDeterminados);
+    int j = 0;
+    while(getValorVerdad() && j < sizeof(valoresDeterminados) / sizeof(valoresDeterminados[0])) {
+        if(valoresDeterminados[j] == false)
+            setValorVerdad(false);
+        j++;
+    }
+}
