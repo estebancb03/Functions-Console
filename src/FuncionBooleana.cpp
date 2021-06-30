d572f1fa2401be1956ec4743cd7bff338452a476
estebancb03 <estebanr.castaneda03@gmail.com>
2021-06-30 01:19:24 -0600

    Correccion en nombre de variable

--- src/FuncionBooleana.cpp
+++ src/FuncionBooleana.cpp
@@ -6,10 +6,11 @@ using namespace std;
 
 void FuncionBooleana :: seleccionarMinterminos() {
     string formula;
-    stringstream ssFormula(formula);
+    stringstream ssFormula(formulaFuncion);
     while(getline(ssFormula, formula, '+')) {
         Mintermino *m = new Mintermino(formula, variableA, variableB, variableC);
         m -> evaluar();
+        cout << m ->getValorVerdad();
         listaMinterminos -> agregarMintermino(m);
     }
 }
