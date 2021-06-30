941aead15e8a83b3bd98395ce1e0a57c4172af8e
estebancb03 <estebanr.castaneda03@gmail.com>
2021-06-29 23:36:52 -0600

    Creacion de los headers de la clase Mintermino

---
+++ src/Mintermino.h
@@ -0,0 +1,22 @@
+#ifndef MINTERMINO_H
+#define MINTERMINO_H
+#include <iostream>
+using namespace std;
+
+class Mintermino {
+    bool introducidoA, introducidoB, introducidoC;
+    bool valorVerdad;
+    string formulaMintermino;
+    public:
+        Mintermino(string fmin, bool a, bool b, bool c) { formulaMintermino = fmin; introducidoA = a; introducidoB = b; introducidoC = c; valorVerdad = true; };
+        ~Mintermino();
+        void evaluar();
+        int longitud(string f);
+        void cambiarValoresVerdad(bool introducidos[], bool determinados[]);
+        void setValorVerdad(bool v) { valorVerdad = v; };
+        bool getValorVerdad() { return valorVerdad; };
+        string getFormula() { return formulaMintermino; };
+};
+
+#endif 
+
