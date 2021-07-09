6ebfc2b74e0d017e5ac62edae75a20385e3ba477
estebancb03 <estebanr.castaneda03@gmail.com>
2021-07-09 16:46:47 -0600

    Se agrega clase Nod en formato de template

---
+++ src/Nodo.h
@@ -0,0 +1,20 @@
+#ifndef NODO_H
+#define NODO_H
+template <class T>
+
+class Nodo {
+    T objeto;
+    bool valorVerdad;
+    Nodo<T> *siguiente;
+    public:
+        Nodo(T o, bool vv) { objeto = o; valorVerdad = vv; siguiente = nullptr; };
+        ~Nodo() { delete objeto; delete siguiente; };
+        Nodo<T>* getSiguiente() { return siguiente; };
+        T getObj() { return objeto; };
+        bool getValorVerdad() { return valorVerdad; };
+        void setSiguiente(Nodo<T> * s) { siguiente = s; };
+        void setObj(T o) { objeto = o; };
+        void setValorVerdad(bool vv) { valorVerdad = vv; };
+};
+
+#endif
