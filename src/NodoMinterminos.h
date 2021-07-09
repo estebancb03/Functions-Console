5eb98d7cf166469957d9c9048214e716d3bfa076
estebancb03 <estebanr.castaneda03@gmail.com>
2021-07-09 01:23:23 -0600

    Se agrega una nueva propiedad y sus headers

--- src/NodoMinterminos.h
+++ src/NodoMinterminos.h
@@ -4,14 +4,17 @@
 
 class NodoMinterminos {
     Mintermino *mintermino;
+    bool valorVerdad;
     NodoMinterminos *siguiente;
     public:
-        NodoMinterminos(Mintermino *m) { mintermino = m; siguiente = nullptr; };
+        NodoMinterminos(Mintermino *m) { mintermino = m; valorVerdad = true; siguiente = nullptr; };
         ~NodoMinterminos() { delete mintermino; delete siguiente; };
         NodoMinterminos *getSiguiente() { return siguiente; };
         Mintermino * getMintermino() { return mintermino; };
+        bool getValorVerdad() { return valorVerdad; };
         void setSiguiente(NodoMinterminos *s) { siguiente = s; };
         void setMintermino(Mintermino *m) { mintermino = m; };
+        void setValorVerdad(bool vv) { valorVerdad = vv; };
 };
 
 #endif
