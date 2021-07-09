d15e90bc57a0503be9d727101478858f3d0d02dc
estebancb03 <estebanr.castaneda03@gmail.com>
2021-07-09 01:12:03 -0600

    Se elimina metodo getValoresvERDAD

--- src/ListaMinterminos.cpp
+++ src/ListaMinterminos.cpp
@@ -36,13 +36,3 @@ int ListaMinterminos :: longitud() {
     }
         return cont;
 }
-
-void ListaMinterminos :: getValoresVerdad(bool arrayVerdades[]) {
-    int i = 0;
-    NodoMinterminos *temp = cabeza;
-    while(temp != nullptr) {
-        arrayVerdades[i] = temp -> getMintermino() -> getValorVerdad();  
-        temp = temp -> getSiguiente();
-        i++;
-    }
-}
