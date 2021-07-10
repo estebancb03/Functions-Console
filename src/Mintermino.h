456960ff3f74b0aabda691091fee72beb58926ca
estebancb03 <estebanr.castaneda03@gmail.com>
2021-07-09 21:14:44 -0600

    Intento por arreglar error undeffined reference to Lista

--- src/Mintermino.h
+++ src/Mintermino.h
@@ -1,17 +1,17 @@
 #ifndef MINTERMINO_H
 #define MINTERMINO_H
+#include <iostream>
 #include "ListaVariables.h"
 #include "Lista.h"
-#include <iostream>
 using namespace std;
 
 class Mintermino {
-    Lista<string> *variables;
+    Lista<string> *variables = new Lista<string>();
     bool introducidoA, introducidoB, introducidoC, introducidoD;
     string formulaMintermino;
     public:
-        Mintermino(string fmin, bool a, bool b, bool c, bool d) { variables = new Lista<string>(); formulaMintermino = fmin; introducidoA = a; introducidoB = b; introducidoC = c; introducidoD = d; };
-        ~Mintermino() { delete variables; };
+        Mintermino(string fmin, bool a, bool b, bool c, bool d) { formulaMintermino = fmin; introducidoA = a; introducidoB = b; introducidoC = c; introducidoD = d; };
+        //~Mintermino() { delete variables; };
         bool evaluar();
         void estandarizar();
         void llenarListaVariables();
