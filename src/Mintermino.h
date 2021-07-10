d96c34aaa216b6a2c22905c4be81fc49f11aa592
estebancb03 <estebanr.castaneda03@gmail.com>
2021-07-09 22:42:05 -0600

    Se agrega nueva propiedada y sus getters y setters

--- src/Mintermino.h
+++ src/Mintermino.h
@@ -7,15 +7,18 @@ using namespace std;
 class Mintermino {
     ListaVariables *variables;
     bool introducidoA, introducidoB, introducidoC, introducidoD;
+    bool valorVerdad;
     string formulaMintermino;
     public:
-        Mintermino(string fmin, bool a, bool b, bool c, bool d) { variables = new ListaVariables(); formulaMintermino = fmin; introducidoA = a; introducidoB = b; introducidoC = c; introducidoD = d; };
+        Mintermino(string fmin, bool a, bool b, bool c, bool d) { variables = new ListaVariables(); formulaMintermino = fmin; introducidoA = a; introducidoB = b; introducidoC = c; introducidoD = d; valorVerdad = false; };
         ~Mintermino() { delete variables; };
         bool evaluar();
         void estandarizar();
         void llenarListaVariables();
         string averiguaFaltantes();
         string getFormula() { return formulaMintermino; };
+        void setValorVerdad(bool vv) { valorVerdad = vv; };
+        bool getValorVeradad() { return valorVerdad; };
 };
 
 #endif 
