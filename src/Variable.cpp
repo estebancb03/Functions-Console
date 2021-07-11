d11db7da7127c15025d2b8545cc6f8ca1309042a
estebancb03 <estebanr.castaneda03@gmail.com>
2021-07-11 11:37:08 -0600

    Cambio estetico

--- src/Variable.cpp
+++ src/Variable.cpp
@@ -3,11 +3,7 @@
 using namespace std;
 
 bool Variable :: evaluar() {
-    if(formula.find("'") != string :: npos) {
-        if(getValorVerdad())
-            setValorVerdad(false);
-        else
-            setValorVerdad(true);
-    }
+    if(formula.find("'") != string :: npos) 
+        setValorVerdad(!getValorVerdad());
     return getValorVerdad();
 }
