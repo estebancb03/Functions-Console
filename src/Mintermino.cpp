c8b019b391d32fd7888d39950baa5a50c2c61066
estebancb03 <estebanr.castaneda03@gmail.com>
2021-07-11 11:28:26 -0600

    Cambios menores en nombres de metodos que se modificaron en otras clases

--- src/Mintermino.cpp
+++ src/Mintermino.cpp
@@ -14,13 +14,13 @@ bool Mintermino :: evaluar() {
     cout << "Formula revisada = " << formulaMintermino << endl;
     for(int i = 0; i < variablesPosibles.length(); i++) {
         letra = variablesPosibles[i];
-        determinados[i] = variables -> getNodoV(letra) -> getObj() -> getValorVerdad();
+        determinados[i] = variables -> getNodo(letra) -> getObj() -> getValorVerdad();
     }
     cout << "Valores revisados = " << determinados[0] << determinados[1] << determinados[2] << determinados[3] << endl;
     int j = 0;
     while(j < variables -> longitud()) {
         if(determinados[j] == false)
-            setValorVerdad(false);;
+            setValorVerdad(false);
         j++;
     }
     cout << "Valor Mintermino = " << getValorVerdad() << endl << endl;
@@ -86,6 +86,6 @@ void Mintermino :: estandarizar() {
     formulaMintermino = "";
     for(int i = 0; i < variablesPosibles.length(); i++){
         letraRecorrido = variablesPosibles[i];
-        formulaMintermino += variables -> getNodoV(letraRecorrido) -> getObj() -> getFormula();
+        formulaMintermino += variables -> getNodo(letraRecorrido) -> getObj() -> getFormula();
     }
 }
