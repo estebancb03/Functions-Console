ab3a09b0bcb56fba129a59e1258da136faf9a459
estebancb03 <estebanr.castaneda03@gmail.com>
2021-07-11 11:14:42 -0600

    Se agregan los metodos en esta clase de encabezados para evitar errores y bugs con los templates

--- src/Lista.h
+++ src/Lista.h
@@ -18,4 +18,63 @@ class Lista {
         int longitud();
 };
 
+template <typename T>
+void Lista<T> :: agregarObjeto(T *o) {
+    Nodo<T> *temp = cabeza;
+    Nodo<T> *nuevo = new Nodo<T>(o);
+    if(temp == nullptr)
+        cabeza = nuevo;
+    else {
+        while(temp -> getSiguiente() != nullptr)
+            temp = temp -> getSiguiente();
+        temp -> setSiguiente(nuevo);
+    }
+}
+
+template <typename T>
+void Lista<T> :: imprimirLista() {
+    Nodo<T> *temp = cabeza;
+    if(temp == nullptr)
+        cout << "Lista vacia";
+    else {
+        while(temp != nullptr) {
+            cout << temp -> getObj() << "->";
+            temp = temp -> getSiguiente();
+        }
+        cout << "NULL";
+    }
+}
+
+template <typename T>
+int Lista<T> :: longitud() {
+    int cont = 0;
+    Nodo<T> *temp = cabeza;
+    while(temp != nullptr) {
+        cont++;
+        temp = temp -> getSiguiente();
+    }
+        return cont;
+}
+
+template <typename T>
+Nodo<T>* Lista<T> :: getNodo(string s) {
+    Nodo<T> *temp = cabeza;
+    while(temp -> getSiguiente() != nullptr && temp -> getObj() -> getFormula().find(s) == string :: npos) {
+        temp = temp -> getSiguiente();
+    }
+    return temp;
+}
+
+template <typename T>
+bool Lista<T> :: encontrar(string s) {
+    bool encontrado = false;
+    Nodo<T> *temp = cabeza;
+    while(encontrado == false && temp != nullptr) {
+        if(temp -> getObj() -> getLetra().find(s) != string :: npos)
+            encontrado = true;
+        temp = temp -> getSiguiente();
+    }
+    return encontrado;
+}
+
 #endif 
